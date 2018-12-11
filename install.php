aaa0378629b31a7579048747347edee9f72c75f2
Charlene Auger <charlene.auger@factorfx.com>
2018-12-11 09:54:30 +0100

    Update plugin for extension engine

--- install.php
+++ install.php
@@ -1,53 +1,52 @@
 <?php
-function plugin_version_scheduledtasks()
+
+/**
+ * This function is called on installation and is used to create database schema for the plugin
+ */
+function extension_install_scheduledtasks()
 {
-return array('name' => 'scheduledtasks',
-'version' => '2.0',
-'author'=> 'Valentin DEVILLE, Guillaume PRIOU',
-'license' => 'GPLv2',
-'verMinOcs' => '2.2');
+    $commonObject = new ExtensionCommon;
+
+    $commonObject -> sqlQuery("CREATE TABLE IF NOT EXISTS `scheduledtasks` (
+                              `ID` INT(11) NOT NULL AUTO_INCREMENT,
+                              `HARDWARE_ID` INT(11) NOT NULL,
+                              `NAME` VARCHAR(255) NOT NULL,
+                              `NEXTEXECHOUR` VARCHAR(255) NOT NULL,
+                              `STATE` VARCHAR(255) NOT NULL,
+                              `LASTEXECHOUR` VARCHAR(255) NOT NULL,
+                              `LASTRESULT` INTEGER DEFAULT NULL,
+                              `CREATOR` VARCHAR(255) NOT NULL,
+                              `PLANNING` VARCHAR(255) NOT NULL,
+                              `TASKTOEXECUTE` VARCHAR(255) NOT NULL,
+                              `STARTIN` VARCHAR(255) NOT NULL,
+                              `COMMENT` VARCHAR(255) NOT NULL,
+                              `TASKSTATUS` VARCHAR(255) NOT NULL,
+                              `TYPE` VARCHAR(255) NOT NULL,
+                              `BEGINHOUR` VARCHAR(255) NOT NULL,
+                              `BEGINDATE` VARCHAR(255) NOT NULL,
+                              `ENDDATE` VARCHAR(255) NOT NULL,
+                              `DAY` VARCHAR(255) NOT NULL,
+                              `MONTH` VARCHAR(255) NOT NULL,
+                              `EXECUTEAS` VARCHAR(255) NOT NULL,
+                              `DELTASK` VARCHAR(255) NOT NULL,
+                              `STOPTASKAFTER` VARCHAR(255) NOT NULL,
+                              PRIMARY KEY (`ID`,`HARDWARE_ID`)
+                              ) ENGINE=INNODB;");
 }
 
-function plugin_init_scheduledtasks()
+/**
+ * This function is called on removal and is used to destroy database schema for the plugin
+ */
+function extension_delete_scheduledtasks()
 {
-$object = new plugins;
-$object -> add_cd_entry("scheduledtasks","other");
-
-$object -> sql_query("
-                    CREATE TABLE IF NOT EXISTS `scheduledtasks` (
-                    `ID` INT(11) NOT NULL AUTO_INCREMENT,
-                    `HARDWARE_ID` INT(11) NOT NULL,
-                    `NAME` VARCHAR(255) NOT NULL, 
-                    `NEXTEXECHOUR` VARCHAR(255) NOT NULL, 
-                    `STATE` VARCHAR(255) NOT NULL, 
-                    `LASTEXECHOUR` VARCHAR(255) NOT NULL, 
-                    `LASTRESULT` INTEGER DEFAULT NULL, 
-                    `CREATOR` VARCHAR(255) NOT NULL,
-                    `PLANNING` VARCHAR(255) NOT NULL, 
-                    `TASKTOEXECUTE` VARCHAR(255) NOT NULL, 
-                    `STARTIN` VARCHAR(255) NOT NULL, 
-                    `COMMENT` VARCHAR(255) NOT NULL,
-                    `TASKSTATUS` VARCHAR(255) NOT NULL,
-                    `TYPE` VARCHAR(255) NOT NULL,
-                    `BEGINHOUR` VARCHAR(255) NOT NULL,
-                    `BEGINDATE` VARCHAR(255) NOT NULL,
-                    `ENDDATE` VARCHAR(255) NOT NULL,
-                    `DAY` VARCHAR(255) NOT NULL,
-                    `MONTH` VARCHAR(255) NOT NULL,
-                    `EXECUTEAS` VARCHAR(255) NOT NULL,
-                    `DELTASK` VARCHAR(255) NOT NULL,
-                    `STOPTASKAFTER` VARCHAR(255) NOT NULL,
-                    PRIMARY KEY (`ID`,`HARDWARE_ID`)
-                    ) ENGINE=INNODB;");
-
+    $commonObject = new ExtensionCommon;
+    $commonObject -> sqlQuery("DROP TABLE `scheduledtasks`;");
 }
 
-function plugin_delete_scheduledtasks()
+/**
+ * This function is called on plugin upgrade
+ */
+function extension_upgrade_scheduledtasks()
 {
-$object = new plugins;
-$object -> del_cd_entry("scheduledtasks");
-$object -> sql_query("DROP TABLE `scheduledtasks`;");
 
 }
-
-?>
